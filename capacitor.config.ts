4a64b04ff52e4db45638b306a0ead11665ee049d
hegig <hegi.gjoka@beta80group.it>
2025-04-21 11:30:18 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mercedes-benz-300',
+  webDir: 'www'
+};
+
+export default config;
